170704c4e3ce0fea15570965e19c6ed29db5f18f
Pola, Sai Mukesh <saimukesh.pola@in.pega.com>
2021-01-28 16:51:38 +0530

    Integrates storybook, styled components and react router

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import "./button.css";
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+const ButtonWrapper = styled.button``;
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <ButtonWrapper
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </ButtonWrapper>
+  );
+};
